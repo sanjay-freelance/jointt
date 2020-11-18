48814ef8da800cb177fc2cd07584c8935967abe8
Sanjay krishna anbalagan <sanjay.krishna@cengage.com>
2020-11-18 13:27:12 -0600

    Devops Bug: webpack generated chuck are using relative path, which causes loading issue when deployed, temp solution is to revert lazy fetching

--- src/abstract/ui/routes/DynamicImport.js
+++ src/abstract/ui/routes/DynamicImport.js
@@ -5,8 +5,11 @@ export default function DynamicImport(props){
 	const {path} = props;
 	const [Component, setComponent] = useState(null);
 
+	// todo: webpack chuck are loaded by http request,
+	// that time the path for these chunks are not relative
+	// temp solution: use eager mode
 	useEffect(()=>{
-		import(`editor/pages/${path}`).then((module)=>{
+		import(/* webpackMode: "eager" */ `editor/pages/${path}`).then((module)=>{
 			const uiComponent = module.default ? module.default : module;
 			setComponent(uiComponent);
 		});
