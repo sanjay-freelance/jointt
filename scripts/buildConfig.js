3b425820838dd6ab7740fad1fe607374b30950d9
Sanjay krishna anbalagan <sanjay.krishna@cengage.com>
2020-11-18 02:00:53 -0600

    script to build and deploy are created

---
+++ scripts/buildConfig.js
@@ -0,0 +1,26 @@
+var config = {
+  "copy":{
+    "html":{
+      "sourceDir": null,
+      "sourceFiles": ["index"],
+      "destinationDir":"dist"
+    },
+    "css":{
+      "sourceDir":null,
+      "sourceFiles": null,
+      "destinationDir":null
+    },
+    "json":{
+      "sourceDir":null,
+      "sourceFiles": null,
+      "destinationDir":null
+    },
+    "other":{
+      "sourceDir":'static',
+      "sourceFiles": null,
+      "destinationDir":'dist/static'
+    }
+  }
+};
+
+module.exports = config;
